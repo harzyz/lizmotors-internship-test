794ff7a38a45468ffaa30e5e03fd0972c6f53176
harzyz <110671828+harzyz@users.noreply.github.com>
2024-03-24 03:21:24 +0100

    hover fix

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Sidebar from "../sidebar/sidebar";
 import styles from "./dashboard.module.css";
+import { IoCarSport } from "react-icons/io5";
 
 const Dashboard = ({ children }) => {
   return (
@@ -9,11 +10,12 @@ const Dashboard = ({ children }) => {
         <Sidebar />
       </div>
       <div className={styles.content}>
-        <div className={styles.topbar}>
-          <Link to="/" className={styles.link}>
+        <Link to="/" className={styles.link}>
+          <div className={styles.topbar}>
+            <IoCarSport size={30} />
             <h1 className={styles.header}>LIZMOTORS</h1>
-          </Link>
-        </div>
+          </div>
+        </Link>
         {children}
       </div>
     </main>
